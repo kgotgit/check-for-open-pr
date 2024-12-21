377f30438949adf87e8e37c1ebf19cfea6878f45
Karthik Gotrala <6868408+kgotgit@users.noreply.github.com>
2024-12-20 18:31:07 -0600

    testing

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,34 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
-
+import * as github from '@actions/github';
+import { PullRequest } from '@octokit/webhooks-types';
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const sha: string = github.context.sha;
+    const token : string = core.getInput('token');
+    
+    const octokit = github.getOctokit(token);
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const { data: pullRequests } = await octokit.rest.pulls.list({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      state: 'open',
+    });
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    console.log(pullRequests);
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const activePR: PullRequest | undefined = (pullRequests as PullRequest[]).find((pr: PullRequest) => pr.head.sha === sha);
+
+    if (activePR) {
+      core.info(`Active pull request found: ${activePR.html_url}`);
+    } else {
+      core.info('No active pull request found for the given SHA.');
+    }
+
+
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
